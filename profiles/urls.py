15764f263875bf10b757c67acb32447069db587e
iamisha <isha.201710@ncit.edu.np>
2024-01-18 20:49:18 +0545

    urls.py

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
     path('signup/', views.signup_view, name='signup'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
