d97293c555a78138c159478e315c0b9de5ef2d56
iamisha <isha.201710@ncit.edu.np>
2024-01-18 20:48:27 +0545

    urls.py

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
     
     path('', views.index, name='home')
+    path('signup/', views.signup_view, name='signup'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('profile/update/', views.update_profile, name='update_profile'),
+
 ]
